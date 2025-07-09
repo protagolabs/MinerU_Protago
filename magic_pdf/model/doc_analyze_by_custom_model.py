60d0246ef9ef906953d0415aba91283bd5cefb06
xingdi1990 <detiondx@hotmail.com>
2025-07-09 13:42:09 -0400

    adjust vram usage

--- magic_pdf/model/doc_analyze_by_custom_model.py
+++ magic_pdf/model/doc_analyze_by_custom_model.py
@@ -262,25 +262,12 @@ def may_batch_image_analyze(
 
     if str(device).startswith('npu') or str(device).startswith('cuda'):
         vram = get_vram(device)
-        # if vram is not None:
-        #     gpu_memory = int(os.getenv('VIRTUAL_VRAM_SIZE', round(vram)))
-        #     if gpu_memory >= 16:
-        #         batch_ratio = 16
-        #     elif gpu_memory >= 12:
-        #         batch_ratio = 8
-        #     elif gpu_memory >= 8:
-        #         batch_ratio = 4
-        #     elif gpu_memory >= 6:
-        #         batch_ratio = 2
-        #     else:
-        #         batch_ratio = 1
-        #     logger.info(f'gpu_memory: {gpu_memory} GB, batch_ratio: {batch_ratio}')
         if vram is not None:
             gpu_memory = int(os.getenv('VIRTUAL_VRAM_SIZE', round(vram)))
             if gpu_memory >= 16:
-                batch_ratio = 8
+                batch_ratio = 16
             elif gpu_memory >= 12:
-                batch_ratio = 6
+                batch_ratio = 8
             elif gpu_memory >= 8:
                 batch_ratio = 4
             elif gpu_memory >= 6:
@@ -288,6 +275,19 @@ def may_batch_image_analyze(
             else:
                 batch_ratio = 1
             logger.info(f'gpu_memory: {gpu_memory} GB, batch_ratio: {batch_ratio}')
+        # if vram is not None:
+        #     gpu_memory = int(os.getenv('VIRTUAL_VRAM_SIZE', round(vram)))
+        #     if gpu_memory >= 16:
+        #         batch_ratio = 8
+        #     elif gpu_memory >= 12:
+        #         batch_ratio = 6
+        #     elif gpu_memory >= 8:
+        #         batch_ratio = 4
+        #     elif gpu_memory >= 6:
+        #         batch_ratio = 2
+        #     else:
+        #         batch_ratio = 1
+        #     logger.info(f'gpu_memory: {gpu_memory} GB, batch_ratio: {batch_ratio}')
         else:
             # Default batch_ratio when VRAM can't be determined
             batch_ratio = 1
